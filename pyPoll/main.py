91c7ee81f87f9e7fec5b81b32daeb40db72739ba
Rylee Nelson <rylee.nelson1397@gmail.com>
2021-01-23 20:02:34 -0600

    Figured out the total votes

--- pyPoll/main.py
+++ pyPoll/main.py
@@ -1,32 +1,45 @@
+# importing environments and csv file
 import os
 import csv
-# the .. makes so that we can open the resources that is on the same level 
-# as our solved/unsolved file
+
+# naming csv file
 election_csv = os.path.join("Resources", "election_data.csv")
+
+#initializing/defining variables
 total_votes = 0
 candidate_list = {}
 candidate_votes = []
+
+#Print title
+print("Election Results")
+
+#Print divider betweeen total votes and title
+print("----------------------")
+
 # Open and read csv
 with open(election_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
 
-    # Read the header row first (skip this part if there is no header)
+    # Read the header row first 
     csv_header = next(csv_file)
-    print(f"Header: {csv_header}")
-
+    
+    # Find and print total votes
+    # I know this says row but it's not, it just won't work when I say column.
     for row in csv_reader:
         total_votes = total_votes + 1
+    
         candidate_name = row[2]
         if candidate_name not in candidate_list:
             #cand_index = candidate_list.index(candidate_name)
             candidate_list[candidate_name] = 1
         else:
             candidate_list[candidate_name] = candidate_list[candidate_name] + 1
-        print(candidate_list)
-
-    for index,candidate_votes in candidate_list.items():
+        #print(candidate_list)
     
-    print(index, candidate_votes)
+    print(f'Total votes: {total_votes}')
+    # for index,candidate_votes in candidate_list.items():
+    
+    # #print(index, candidate_votes)
 
-    print(f'Total Votes:  {total_votes}')
+    # print(f'Total Votes:  {total_votes}')
 
