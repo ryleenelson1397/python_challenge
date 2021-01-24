350de4143b9bee269c1845d81296c422fb72d6e5
Rylee Nelson <rylee.nelson1397@gmail.com>
2021-01-23 20:48:16 -0600

    mostly finished pyPoll

--- pyPoll/main.py
+++ pyPoll/main.py
@@ -7,8 +7,17 @@ election_csv = os.path.join("Resources", "election_data.csv")
 
 #initializing/defining variables
 total_votes = 0
-candidate_list = {}
+candidates = []
 candidate_votes = []
+kahn_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
+kahn_percent = 0
+correy_percent = 0
+li_percent = 0
+otooley_percent = 0
+winner = []
 
 #Print title
 print("Election Results")
@@ -24,22 +33,51 @@ with open(election_csv) as csv_file:
     csv_header = next(csv_file)
     
     # Find and print total votes
-    # I know this says row but it's not, it just won't work when I say column.
+ 
     for row in csv_reader:
-        total_votes = total_votes + 1
+        total_votes += 1
     
-        candidate_name = row[2]
-        if candidate_name not in candidate_list:
-            #cand_index = candidate_list.index(candidate_name)
-            candidate_list[candidate_name] = 1
-        else:
-            candidate_list[candidate_name] = candidate_list[candidate_name] + 1
-        #print(candidate_list)
-    
-    print(f'Total votes: {total_votes}')
-    # for index,candidate_votes in candidate_list.items():
-    
-    # #print(index, candidate_votes)
+        
+        if row[2] not in candidates:
+            candidates.append(row[2])
+        
+        # Finding total votes for each candidate
+        if row[2] == "Khan":
+            kahn_votes += 1
+        
+        elif row[2] == "Correy":
+            correy_votes += 1
 
-    # print(f'Total Votes:  {total_votes}')
+        elif row[2] == "Li":
+            li_votes += 1
+
+        elif row[2] == "O'Tooley":
+            otooley_votes += 1
+
+    #printing the total amount of votes
+    print(f'Total votes: {total_votes}')
+
+    #printing divider between total votes and candidate summary
+    print("----------------------")
+
+    # Calculation of the percecntages of votes of each candidate
+    kahn_percent = "{:.3%}".format(kahn_votes/total_votes)
+
+    correy_percent = "{:.3%}".format(correy_votes/total_votes)
+
+    li_percent = "{:.3%}".format(li_votes/total_votes)
+
+    otooley_percent = "{:.3%}".format(otooley_votes/total_votes)
+
+    # Printing candidate outcomes
+    print(f'Khan: {kahn_percent, kahn_votes}')
+
+    print(f'Correy: {correy_percent, correy_votes}')
+
+    print(f'Li: {li_percent, li_votes}')
+    
+    print(f'OTooley: {otooley_percent, otooley_votes}')
+
+    #Printing divider
+    print("----------------------")
 
