745d89ef5912ee7890f01a97c742d90c10d549e4
Rylee Nelson <rylee.nelson1397@gmail.com>
2021-01-23 19:38:13 -0600

    Solved pyBank and partially solved pyPoll

--- pyBank/main.py
+++ pyBank/main.py
@@ -5,35 +5,12 @@ import csv
 
 # Read CSV file 
 csvpath = os.path.join('Resources', 'budget_data.csv')
-total_profit = []
-total_pl = 0
-date = []
-pl_change = []
-avg_change = 0
-# Reading using the CSV module
-with open(csvpath) as csvfile:
- 
-    #separates values at the comma
-    csvreader = csv.reader(csvfile, delimiter = ',')
 
-    #skip the header row
-    csv_header = next(csvreader)
-    
-    #confirmation that the entire file is read
-    for row in csvreader:
-       total_pl = total_pl + int(row[1]) 
-       print(row)
-       total_profit.append(int(row[1]))
-       
-       date.append(row[0])
-       #print(date, total_profit)
-    print(total_pl)
-    for change in range(1, len(total_profit)):
-        pl_change.append(int(total_profit[change]) - int(total_profit[change-1]))
-    
-    avg_change = sum(pl_change)/ len(pl_change)
-    print(round(avg_change, 2))
+#Creating Title for output
+print("Financial Analysis")
 
+#Creating divider under title
+print("-------------------")
 
 #find the total number of months in the data set
 with open(csvpath) as csvfile:
@@ -43,31 +20,62 @@ with open(csvpath) as csvfile:
 
     #skipping header row and defining total months as a sum
     csv_header = next(csvreader)
-    print(f'Budget Data Header: {csv_header}')
-    
+   
     total_months = sum(1 for row in csvreader)
 
-    print(f'Number of Months: {total_months}')
+    #printing Total Months amount
+    print(f'Total Months: {total_months}')
 
-   
+# Defining/initializing variables for profits/losses column
+
+total_profit = []
+total_pl = 0
+date = []
+pl_change = []
+avg_change = 0
+
+# Reading using the CSV module
+with open(csvpath) as csvfile:
+ 
+    #separates values at the comma
+    csvreader = csv.reader(csvfile, delimiter = ',')
+
+    #skip the header row
+    csv_header = next(csvreader)
     
-    #for row in csvreader:
-        
-        #print(row)
+    # making sure the profit loss and net profit are calculated within the 2nd column
+    # P.S. I know this says row but I couldn't get it to work when I typed column 
+    # so it's row for now
+
+    for row in csvreader:
+       total_pl = total_pl + int(row[1]) 
        
-    # for number in total_profit:
-    #     total_pl += number 
-    #     #print(number)
-    #print(total_pl)    
-# Finding net total amount of profit/losses over entire data set,
-# then finding the average of the changes
-#with open(csvpath) as csvfile:
+       total_profit.append(int(row[1]))
+       
+       date.append(row[0])
 
-    #separate values at commas
-    #csvreader =csv.reader(csvfile, delimiter = ",")
-    
-    #skipping header row and defining net profit/loss over entire data set
-    #csv_header = next(csvreader)
-    #net_profit_or_loss = sum(value for )
-    
+    # Printing net/total profit/losses over the entire period
+    print(f'Total : ${total_pl}')
 
+    for change in range(1, len(total_profit)):
+        pl_change.append(int(total_profit[change]) - int(total_profit[change-1]))
+    
+    # Functions to average change and printing average change
+    avg_change = sum(pl_change)/ len(pl_change)
+
+    print(f'Average Change: ${round(avg_change, 2)}')
+
+    # Defining greatest increase/decrease in profits
+    max_increase = max(pl_change)
+
+    min_increase = min(pl_change)
+
+    date_of_max = date[pl_change.index(max_increase)+1]
+
+    date_of_min = date[pl_change.index(min_increase)+1]
+
+    #print(max_increase, min_increase, date_of_max, date_of_min)
+    print(f'Greatest Increase in Profits: {date_of_max, max_increase}')
+
+    print(f'Greatest Decrease in Profits: {date_of_min, min_increase}')
+    
--- pyPoll/main.py
+++ pyPoll/main.py
@@ -3,7 +3,9 @@ import csv
 # the .. makes so that we can open the resources that is on the same level 
 # as our solved/unsolved file
 election_csv = os.path.join("Resources", "election_data.csv")
-
+total_votes = 0
+candidate_list = {}
+candidate_votes = []
 # Open and read csv
 with open(election_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
@@ -11,3 +13,20 @@ with open(election_csv) as csv_file:
     # Read the header row first (skip this part if there is no header)
     csv_header = next(csv_file)
     print(f"Header: {csv_header}")
+
+    for row in csv_reader:
+        total_votes = total_votes + 1
+        candidate_name = row[2]
+        if candidate_name not in candidate_list:
+            #cand_index = candidate_list.index(candidate_name)
+            candidate_list[candidate_name] = 1
+        else:
+            candidate_list[candidate_name] = candidate_list[candidate_name] + 1
+        print(candidate_list)
+
+    for index,candidate_votes in candidate_list.items():
+    
+    print(index, candidate_votes)
+
+    print(f'Total Votes:  {total_votes}')
+
